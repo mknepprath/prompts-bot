84e81c0c67ce4655f1adda26abb92f738160811c
Michael Knepprath <michael@mknepprath.com>
2016-09-06 22:28:44 -0500

    gets random result out of 305423 words

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ class TwitterAPI:
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
-    offset = str(random.choice(range(20000))) # should be number of words in dictionary
+    offset = str(random.choice(range(305423))) # should be number of words in dictionary
     dictionary = urlopen('https://api.pearson.com/v2/dictionaries/ldoce5/entries?offset=' + offset + '&limit=1&apikey=' + os.environ.get('DICT_CONSUMER_KEY')).read().decode('utf8')
 
     dictData = json.loads(dictionary)
