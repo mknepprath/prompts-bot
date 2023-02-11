3494c71f7e704d26fc479ccd5cd01f488f19e9a0
Michael Knepprath <mknepprath@gmail.com>
2023-02-11 15:25:09 -0500

    Migrate to Mastodon.

--- bot.py
+++ bot.py
@@ -2,27 +2,7 @@ import os
 import json
 import random
 
-import tweepy
 from mastodon import Mastodon
-
-
-class TwitterAPI:
-    """
-    Class for accessing the Twitter API.
-    """
-
-    def __init__(self):
-        consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
-        consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-        access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-        auth.set_access_token(access_token, access_token_secret)
-        self.api = tweepy.API(auth)
-
-    def tweet(self, message):
-        """Send a tweet"""
-        self.api.update_status(status=message)
 
 
 def main():
@@ -40,8 +20,6 @@ def main():
     )
     mastodon.status_post(status=prompt)
 
-    twitter = TwitterAPI()
-    twitter.tweet(prompt)
 
 if __name__ == "__main__":
     main()
